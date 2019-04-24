731408d5218607a3cd843fc7fb49f454dbe26810
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-24 18:12:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -33,8 +33,9 @@ end
  
 
   def sort(grade, student)
-  # school.sort gives an alph. array of student names within a hash, where the student names are the values and grade is the key
+  # school.sort gives an alphzd. array of student names within a hash, where the student names are the values and grade is the key
   # => { key (grade) => ["value", "value"]}
+  # can't just shovel into a hash so easily
   
     sorted = {}
  
